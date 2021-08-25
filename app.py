3bb353bb4484e69b4cb6eb01031799cd88a7a437
Jeeva Pranesh <jpranesh14@gmail.com>
2021-08-25 12:35:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from APIs.Auth import auth_api
 app = Flask(__name__)
 app.register_blueprint(auth_api)
 
+@app.route('/')
 def home():
     return '<h1>Welcome to the Home Page. Application is Under construction.</h1>'
 
